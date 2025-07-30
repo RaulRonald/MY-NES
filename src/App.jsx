630daafa85f4338e27b1a02d5b15536f99ed98a3
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2025-07-30 15:21:30 -0300

    Muda para HashRouter para compatibilidade com GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <HashRouter>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
+    </HashRouter>
   );
 }
 
