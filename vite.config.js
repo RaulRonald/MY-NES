9312cf395e31c2c2252212b042bc913f70631b65
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2025-08-11 19:21:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/' : '/MY-NES/';
+  const base = command === 'serve' ? '/.' : '/MY-NES/.';
 
   return {
     plugins: [react()],
